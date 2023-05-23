5151cbbb2d348cbf60e1ca9f1f730244a6edcc18
VictorMBB <vmbb85@gmail.com>
2023-05-23 15:49:41 -0300

    inicio do cod js

---
+++ jsgradi.js
@@ -0,0 +1,9 @@
+const bntArirDeck = document.querySelector("#card-button");
+bntArirDeck.addEventListener("click", exibirCards);
+
+function exibirCards () {
+    console.log("funciona o click eee");
+}
+
+
+
