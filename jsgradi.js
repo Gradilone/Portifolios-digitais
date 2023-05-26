3ea10c6f67bcebf9905178cf9bdca3a89cc09b82
Gradilone Lucas <lucasgradilone@gmail.com>
2023-05-26 00:02:18 -0300

    javascript da pag gradi e home criada

--- jsgradi.js
+++ jsgradi.js
@@ -1,9 +1,25 @@
-const bntArirDeck = document.querySelector("#card-button");
-bntArirDeck.addEventListener("click", exibirCards);
+const bntArirDeck = document.querySelector("#bnt-interno");
+const cards = document.querySelector("#on");
 
-function exibirCards () {
-    console.log("funciona o click eee");
-}
+cards.style.opacity = '0';
+bntArirDeck.addEventListener("click", function exibirCards () {
+    if (cards.style.opacity == '0'){
+    bntArirDeck.innerHTML = "Fechar meu deck";
+    cards.style.opacity = '1';
+    cards.style.transition = 'opacity 3s';
+    console.log("abrir deck");
+    }
+    else{
+        bntArirDeck.innerHTML = "Abrir meu deck";
+        cards.style.opacity = '0';
+        cards.style.transition = 'opacity 3s';
+        console.log("fechar deck");
+    }
+});
+
+
+
+
 
 
 
---
+++ jshome.js
@@ -0,0 +1,6 @@
+const linkParaOutros = document.querySelector("#gradi");
+
+linkParaOutros.addEventListener("click", function mudarPag () {
+    window.location.href = 'gradi.html';    
+    console.log('cliclou');
+});
