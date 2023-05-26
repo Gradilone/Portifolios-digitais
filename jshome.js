4a28c51bcee8bd3f8eabb3609c32f78942fc63ea
Gradilone <lucasgradilone@gmail.com>
2023-05-26 14:04:08 -0300

    pag gradi finalizada e outra implementações

--- jshome.js
+++ jshome.js
@@ -1,6 +1,19 @@
-const linkParaOutros = document.querySelector("#gradi");
+const btnGradi = document.querySelector("#gradi");
 
-linkParaOutros.addEventListener("click", function mudarPag () {
+btnGradi.addEventListener("click", function mudarPag () {
     window.location.href = 'gradi.html';    
     console.log('cliclou');
+});
+
+
+btnGradi.addEventListener('mouseover', function() {
+
+  const pai = btnGradi.parentNode;
+
+  const irmaos = Array.from(pai.children);
+  irmaos.forEach(function(irmao) {
+    if (irmao !== btnGradi) {
+      pai.removeChild(irmao);
+    }
+  });
 });
